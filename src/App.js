32e0bad382435c6616a4d8a71e3628837ff74f25
Brad <brad@viziworx.com>
2019-10-07 16:43:09 -0400

    Set up the state value for the Tigers team score using a second state hook call

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
   const [countHome, setCountHome] = useState(32);
+  const [countAway, setCountAway] = useState(32);
 
   return (
     <div className="container">
